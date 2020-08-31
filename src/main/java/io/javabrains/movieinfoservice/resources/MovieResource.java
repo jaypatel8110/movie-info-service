9d5c35460a103a03055fca691d0b2798c4f596fa
Jay Patel <jpatel8110@gmail.com>
2020-08-31 00:04:00 -0400

    Api Call to movie Db Implimented

--- src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
+++ src/main/java/io/javabrains/movieinfoservice/resources/MovieResource.java
@@ -1,18 +1,32 @@
 package io.javabrains.movieinfoservice.resources;
 
 import io.javabrains.movieinfoservice.models.Movie;
+import io.javabrains.movieinfoservice.models.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
 
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-        return new Movie(movieId, "Movie called");
-
+        try {
+            MovieSummary movieSummary = restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey, MovieSummary.class);
+            return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+        }catch (Exception e){
+            System.out.println("Exception in Movie in calling external API" +e);
+            return new Movie();
+        }
     }
 
 }
